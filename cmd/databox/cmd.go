fc491a009c75639164256c28179b7bcc746e446e
Yurii Zinets <yurii.zinets@icloud.com>
2026-04-19 13:39:34 +0200

    format

--- cmd/databox/cmd.go
+++ cmd/databox/cmd.go
@@ -10,16 +10,16 @@ type SubCommand struct {
 }
 
 var SubCommands = map[string]SubCommand{
-	"dsn":   {dsnFlagSet, dsnUsage, dsnDescr, dsnCmd},
-	"ls":    {lsFlagSet, lsUsage, lsDescr, lsCmd},
-	"sql":   {sqlFlagSet, sqlUsage, sqlDescr, sqlCmd},
-	"cat":   {catFlagSet, catUsage, catDescr, catCmd},
-	"grep":  {grepFlagSet, grepUsage, grepDescr, grepCmd},
-	"head":  {headFlagSet, headUsage, headDescr, headCmd},
-	"tail":  {tailFlagSet, tailUsage, tailDescr, tailCmd},
-	"count": {countFlagSet, countUsage, countDescr, countCmd},
+	"dsn":     {dsnFlagSet, dsnUsage, dsnDescr, dsnCmd},
+	"ls":      {lsFlagSet, lsUsage, lsDescr, lsCmd},
+	"sql":     {sqlFlagSet, sqlUsage, sqlDescr, sqlCmd},
+	"cat":     {catFlagSet, catUsage, catDescr, catCmd},
+	"grep":    {grepFlagSet, grepUsage, grepDescr, grepCmd},
+	"head":    {headFlagSet, headUsage, headDescr, headCmd},
+	"tail":    {tailFlagSet, tailUsage, tailDescr, tailCmd},
+	"count":   {countFlagSet, countUsage, countDescr, countCmd},
 	"cp":      {cpFlagSet, cpUsage, cpDescr, cpCmd},
 	"migrate": {migrateFlagSet, migrateUsage, migrateDescr, migrateCmd},
-	"ps":    {psFlagSet, psUsage, psDescr, psCmd},
-	"kill":  {killFlagSet, killUsage, killDescr, killCmd},
+	"ps":      {psFlagSet, psUsage, psDescr, psCmd},
+	"kill":    {killFlagSet, killUsage, killDescr, killCmd},
 }
